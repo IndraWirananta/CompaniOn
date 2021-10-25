8a53eebb12ac324d38ac44b6906bef419a873478
indrawirananta <indra21112000@gmail.com>
2021-10-25 22:58:49 +0800

    fix bugs relating to responsiveness and three js modeling, transition between header and trailing created

--- assets/js/dog.js
+++ assets/js/dog.js
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
 var controls
+var mobileview = false;
 
 const canvas = document.querySelector('.dog_canvas')
 const scene = new THREE.Scene()
@@ -24,15 +25,27 @@ loader.load('assets/3d/dog_new.glb',function(glb){
 const ambientLight = new THREE.AmbientLight(0xcccfff, 1)
 scene.add(ambientLight)
 
-const sizes = {
-    width: window.innerWidth/2,
-    height: window.innerHeight
+
+
+
+
+
+var sizes = {
+    width : window.innerWidth/2,
+    height : window.innerHeight
 }
 
-// const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height, 0.1, 100 )
+if (window.innerWidth <= 600){
+    console.log("OPENED ON MOBILE")
+    sizes.width = window.innerWidth
+    mobileview = true
+}
+
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height, 0.1, 100 )
 camera.position.set(1.4,0.6,1.7)
+
 scene.add(camera)
+
 
 
 const renderer = new THREE.WebGLRenderer({
@@ -44,24 +57,38 @@ controls = new OrbitControls(camera, renderer.domElement)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 renderer.shadowMap.enabled = false
-// ambientLight.shadow.mapSize.width = 512; // default
-// light.shadow.mapSize.height = 512; // default
-// light.shadow.camera.near = 0.5; // default
-// light.shadow.camera.far = 500; 
+controls.enableZoom = false
 renderer.gammaOutput = true
 renderer.render(scene,camera)
 controls.enablePan = false
 controls.autoRotate = true
-
+if (window.innerWidth <= 600){
+    controls.enableZoom = false;
+    controls.enabled = false;
+}
+console.log(sizes.width, sizes.height)
 function animate(){
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
     controls.update()
 }
 function onResize() {
-    sizes.width = window.innerWidth/2,
-    sizes.height = window.innerHeight
+    
+    if (window.innerWidth <= 600){
+        if(mobileview == false){
+            sizes.width = window.innerWidth
+            sizes.height = window.innerHeight
+            controls.enableRotate = false;
+            controls.enabled = false;
+        }
+    }else{
+        sizes.width = window.innerWidth/2
+        sizes.height = window.innerHeight
+        controls.enableRotate = true;
+        controls.enabled = true;
+    }
     renderer.setSize(sizes.width, sizes.height)
+    console.log(sizes.width, sizes.height)
     camera.aspect = sizes.width/sizes.height
     camera.updateProjectionMatrix()
 }
